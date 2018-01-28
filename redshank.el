f3eef7d4891570e6bcb74eb52c73edb765a639b8
Mats Lidell <mats.lidell@cag.se>
2018-01-28 22:35:01 +0100

    Add license line GPL

--- redshank.el
+++ redshank.el
@@ -7,6 +7,8 @@
 ;; Author: Michael Weber <michaelw@foldr.org>
 ;; Keywords: languages, lisp
 
+;; License: GPL
+
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation;
